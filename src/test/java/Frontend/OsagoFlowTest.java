c13aa695307245a48dd178a7c557f88343068f44
Lamskov <dima.lamskoff@yandex.ru>
2025-08-24 16:33:52 +0300

    Test Jenkins

--- src/test/java/Frontend/OsagoFlowTest.java
+++ src/test/java/Frontend/OsagoFlowTest.java
@@ -80,7 +80,7 @@ public class OsagoFlowTest {
         $x("//input[@test-id='bodyNumberControlName']").setValue("NZE1419116311");
         $x("//input[@name='stsNumberControlName']").setValue("12 31 231321");
         $x("//input[@test-id='stsDateControlName']").setValue("01.01.2012");
-        $x("//button[@test-id='continueBtnCarDataComponent']")
+        $x("//button[@class='btn btn-primary btn-lg w-100']")
                 .shouldBe(enabled)
                 .click();
 
